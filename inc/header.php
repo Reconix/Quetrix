7014d0abfb8fdf2e45f3c263fdcf9b7299190685
Reconix <Reconix@users.noreply.github.com>
2023-02-10 23:41:42 +0000

    Update header.php
    Hello dirty drop down, nice to see you

--- inc/header.php
+++ inc/header.php
@@ -16,11 +16,115 @@
           <li class="nav-item">
             <a class="nav-link" href="#">News</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Drop Down</a>
+<style>
+
+
+
+  @media (max-width: 1199.98px) {
+    header .navbar .nav-link {
+      padding: 20px 1px; } }
+
+  header .navbar .nav-link.active {
+    color: #fff !important; }
+
+header .navbar .dropdown-menu {
+  font-size: 14px;
+  border-radius: 4px;
+  border: none;
+  -webkit-box-shadow: 0 2px 30px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 30px 0px rgba(0, 0, 0, 0.2);
+  min-width: 13em;
+  margin-top: -10px; }
+
+
+
+    @media (max-width: 991.98px) {
+      header .navbar .dropdown-menu:before {
+        display: none; } }
+
+  header .navbar .dropdown-menu .dropdown-item:hover {
+    background: #007bff;
+    color: #fff; }
+
+  header .navbar .dropdown-menu .dropdown-item.active {
+    background: #007bff;
+    color: #fff; }
+
+  header .navbar .dropdown-menu a {
+    padding-top: 7px;
+    padding-bottom: 7px; }
+
+</style>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#">Services</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Food Catering</a>
+              <a class="dropdown-item" href="#">Drink &amp; Beverages</a>
+              <a class="dropdown-item" href="#">Wedding &amp; Birthday</a>
+            </div>
           </li>
+
+<style>
+.dropbtn {
+  margin-top: 5px;
+  background-color: transparent;
+  color: var(--bs-nav-link-color);
+  border: none;
+  display: block;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+header .navbar .dropdown-menu:before {
+  bottom: 100%;
+  left: 10%;
+  border: solid transparent;
+  content: " ";
+  height: 0;
+  width: 0;
+  position: absolute;
+  pointer-events: none;
+  border-bottom-color: #fff;
+  border-width: 7px; }
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-menu a:hover {background-color: #ddd;}
+
+.dropdown:hover .dropdown-menu {display: block;}
+
+.dropdown:hover .dropbtn {color: #fff; padding-top: 6px}
+</style>
+
+<div class="nav-item dropdown">
+  <a class="nav-link dropbtn" href="#">Services</a>
+  <div class="dropdown-menu">
+    <a class="nav-link" href="#">Link 1</a>
+    <a class="nav-link" href="#">Link 2</a>
+    <a class="nav-link" href="#">Link 3</a>
+  </div>
+</div>
+
           <li class="nav-item">
-            <a class="nav-link" href="#">Get In Touch</a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <!--
           <li class="nav-item">
